9debc1f3c4cb925ec6dce508d4dcdc703b55c52f
apurva1706 <apurva.peri2001@gmail.com>
2020-10-01 17:53:33 +0530

    Updates

---
+++ precache-manifest.63f2e57e7c21107b1c3510ef3e55cbd0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f03057c69acf6fbb0f00514c94885e",
+    "url": "/apurva/index.html"
+  },
+  {
+    "revision": "a98c78e6d5de9cd56a02",
+    "url": "/apurva/static/css/main.0b563076.chunk.css"
+  },
+  {
+    "revision": "59c74a3664a3ce099cbe",
+    "url": "/apurva/static/js/2.aaa2c581.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apurva/static/js/2.aaa2c581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a98c78e6d5de9cd56a02",
+    "url": "/apurva/static/js/main.2d162830.chunk.js"
+  },
+  {
+    "revision": "6cdd1fd9c6320204eca9",
+    "url": "/apurva/static/js/runtime-main.8746822b.js"
+  },
+  {
+    "revision": "cf5cf2697739b9a55a12560d3a99e099",
+    "url": "/apurva/static/media/pexels-photo-255379.cf5cf269.jpeg"
+  },
+  {
+    "revision": "685be5885fa6351d263902b616a27269",
+    "url": "/apurva/static/media/profile.685be588.jpeg"
+  }
+]);
