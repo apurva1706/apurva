9d0affcf74f2d53c1dd75fdc25b5b9a4885d216a
apurva1706 <apurva.peri2001@gmail.com>
2020-10-02 19:30:02 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './images/profile pic.jpeg';
 import {FaInstagram} from 'react-icons/fa';
 import {FaGithub} from 'react-icons/fa';
 import {FaLinkedin} from 'react-icons/fa';
+import {FaHackerrank} from 'react-icons/fa';
+import {FaEnvelope} from 'react-icons/fa';
 import './App.css';
 
 function App() {
@@ -20,20 +22,34 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         ><FaInstagram /></a>
-        <span>  </span>
+        <span>   </span>
         <a
           href="https://github.com/apurva1706/"
           className="icon-button"
           target="_blank"
           rel="noopener noreferrer"
         ><FaGithub /></a>
-        <span>  </span>
+        <span>   </span>
         <a
           href="https://www.linkedin.com/in/apurva-periwal-2873221b3/"
           className="icon-button"
           target="_blank"
           rel="noopener noreferrer"
         ><FaLinkedin /></a>
+        <span>   </span>
+        <a
+          href="https://www.hackerrank.com/apurva_peri2001"
+          className="icon-button"
+          target="_blank"
+          rel="noopener noreferrer"
+        ><FaHackerrank /></a>
+        <span>   </span>
+        <a
+          href="mailto:apurva.peri2001@gmail.com"
+          className="icon-button"
+          target="_blank"
+          rel="noopener noreferrer"
+        ><FaEnvelope /></a>
         </section>
       </header>
     </div>
